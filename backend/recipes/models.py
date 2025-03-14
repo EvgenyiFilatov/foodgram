36880f51282e5a67ab34b40f51b38e1520b05ab7
Evgenyi Filatov <bleidin12345@yandex.ru>
2025-03-14 23:48:33 +0700

    change model recipes

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from myprofile.models import MyProfile
 
 
@@ -76,7 +77,8 @@ class Recipes(models.Model):
     is_in_shopping_cart = models.BooleanField(default=False)
     created_at = models.DateTimeField(
         auto_now_add=True,
-        verbose_name='Дата публикации'
+        verbose_name='Дата публикации',
+        default=timezone.now
     )
     short_link = models.CharField(max_length=10, unique=True, blank=True)
 
