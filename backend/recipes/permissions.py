e726b0a70f99552ddacc4a41610fb004339b5a03
Evgenyi Filatov <bleidin12345@yandex.ru>
2025-03-15 21:58:22 +0700

    edit permission

--- backend/recipes/permissions.py
+++ backend/recipes/permissions.py
@@ -3,5 +3,7 @@ from rest_framework import permissions
 
 class IsAuthorOrAdmin(permissions.BasePermission):
 
-    def has_object_permission(self, request, view, obj):
-        return obj.author == request.user or request.user.is_staff
+    def has_permission(self, request, view):
+        if request.user.is_staff:
+            return True
+        return request.user == view.get_object().author
