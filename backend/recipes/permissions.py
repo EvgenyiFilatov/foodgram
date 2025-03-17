2783eb276a11023e7244f0e17e27f73db6b48ac1
Evgenyi Filatov <bleidin12345@yandex.ru>
2025-03-17 11:47:17 +0700

    edit permission

--- backend/recipes/permissions.py
+++ backend/recipes/permissions.py
@@ -1,7 +1,10 @@
 from rest_framework import permissions
 
 
-class IsAuthor(permissions.BasePermission):
+class IsAuthorOrAdminOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        return obj.author == request.user
+        return (
+            obj.author == request.user
+            or request.method in permissions.SAFE_METHODS
+            or request.user.is_superuser)
