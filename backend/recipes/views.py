5c346814f0251c409921e800f662070278aa3b82
Evgenyi Filatov <bleidin12345@yandex.ru>
2025-03-17 16:05:24 +0700

    fix bugs

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -81,10 +81,6 @@ class RecipesViewSet(viewsets.ModelViewSet):
     def get_short_link(self, request, pk=None):
         """Получить короткую ссылку на рецепт."""
         recipe = get_object_or_404(Recipes, id=pk)
-        # if not recipe.short_link:
-        #    recipe_hash = hashlib.md5(f"{recipe.id}".encode()).hexdigest()[:6]
-        #     recipe.short_link = recipe_hash
-        #     recipe.save()
         short_link = request.build_absolute_uri(f"/s/{recipe.short_link}")
         return Response({"short-link": short_link})
 
