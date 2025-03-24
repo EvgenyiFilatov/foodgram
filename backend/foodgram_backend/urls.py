50c2439aecc36f827cd8137926a74fb13670805c
Evgenyi Filatov <bleidin12345@yandex.ru>
2025-03-24 13:24:23 +0700

    fix bugs

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from api.views import redirect_short_link
+from api.views import RecipesViewSet, redirect_short_link
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,6 +12,11 @@ urlpatterns = [
         's/<str:short_link>/',
         redirect_short_link,
         name='redirect_short_link'
+    ),
+    path(
+        'recipes/<int:pk>/',
+        RecipesViewSet.as_view({'get': 'retrieve'}),
+        name='recipes'
     ),
 ]
 
